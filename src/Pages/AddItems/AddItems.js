139d8bda1e6373b70bcf6377d7b6d020f09c89da
raiansakil <aburaian182@gmail.com>
2022-05-09 18:29:39 +0600

    added item

--- src/Pages/AddItems/AddItems.js
+++ src/Pages/AddItems/AddItems.js
@@ -6,7 +6,7 @@ const AddItems = () => {
 
   const onSubmit = (data) => {
     console.log(data);
-    const url = `http://localhost:5000/product`;
+    const url = `https://stark-coast-67001.herokuapp.com/product`;
     fetch(url, {
       method: "POST",
       headers: {
