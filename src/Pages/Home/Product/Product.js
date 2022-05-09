4a51d13168cd9918cbbc17e5b19b7f406a8eca24
raiansakil <aburaian182@gmail.com>
2022-05-09 22:10:30 +0600

    update

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -18,7 +18,7 @@ const Product = ({ product }) => {
   const handleDelete = (id) => {
     const proceed = window.confirm("Are you sure?");
     if (proceed) {
-      const url = `http://localhost:5000/product/${id}`;
+      const url = `https://stark-coast-67001.herokuapp.com/product/${id}`;
       fetch(url, {
         method: "DELETE",
       })
